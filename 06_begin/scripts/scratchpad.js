59cc7019181476e601e6315590f6d186b30b09ba
tomm <so4raycpw@moakt.co>
2017-10-28 06:48:38 +0200

    Lesson6 video done

---
+++ 06_begin/scripts/scratchpad.js
@@ -0,0 +1,48 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+ */
+
+console.log('kook');
+2+'9';
+/*
+29
+*/
+"hello"+" "+"this is life";
+/*
+hello this is life
+*/
+22<4;
+/*
+false
+*/
+typeof("hazd")
+/*
+string
+*/;
+typeof(300/2);
+/*
+number
+*/
+typeof("hello"+"world");
+/*
+string
+*/
+typeof(4<2);
+/*
+boolean
+*/
+typeof(a+b);
+/*
+Exception: ReferenceError: a is not defined
+@Scratchpad/2:1:1
+*/
+typeof(3+"6");
+/*
+string
+*/
+
