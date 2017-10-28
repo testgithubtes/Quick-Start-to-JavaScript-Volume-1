0271b0d2260430e1fa17b971cc7245ffca85ea8c
tomm <so4raycpw@moakt.co>
2017-10-28 09:33:07 +0200

    Lesson7/8 video done

---
+++ 07_begin/scripts/main.js
@@ -0,0 +1,18 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+ */
+
+var a =2;
+typeof(a);
+var chaine = "test variable";
+alert(chaine);
+var myFavSport =prompt("what is your fav sport");
+alert("my fav sport is"+" "+myFavSport);
+console.log(myFavSport);
+var randomNumber = Math.random()*100;
+alert(randomNumber)
